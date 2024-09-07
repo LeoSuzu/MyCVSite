3478ff105d7b231f1168ebcf018a9b49aa5990fb
LeoSuzu <leona.suzuki@tuni.fi>
2024-09-07 11:34:20 +0300

    JS file fixed more readable

--- js/typed.js
+++ js/typed.js
@@ -1,179 +1,204 @@
-!function(t) {
+!function($) {
     "use strict";
-    const s = function(s, o) {
-        this.el = t(s);
-        this.options = t.extend({}, t.fn.typed.defaults, o);
-        this.isInput = this.el.is("input");
-        this.attr = this.options.attr;
-        this.showCursor = this.isInput ? !1 : this.options.showCursor;
-        // this.elContent = this.attr ? this.el.attr(this.attr) : this.el.text();
-        this.contentType = this.options.contentType;
-        this.typeSpeed = this.options.typeSpeed;
-        this.startDelay = this.options.startDelay;
-        this.backSpeed = this.options.backSpeed;
-        this.backDelay = this.options.backDelay;
-        this.strings = this.options.strings;
-        this.strPos = 0;
-        this.arrayPos = 0;
-        this.stopNum = 0;
-        this.loop = this.options.loop;
-        this.loopCount = this.options.loopCount;
-        this.curLoop = 0;
-        this.stop = !1;
-        this.cursorChar = this.options.cursorChar;
+
+    // Constructor function for Typed effect
+    const TypedEffect = function(element, options) {
+        this.element = $(element);  // Target HTML element
+        this.options = $.extend({}, $.fn.typed.defaults, options);  // Merging default and user-defined options
+        this.isInput = this.element.is("input");  // Check if the element is an input field
+        this.attribute = this.options.attr;  // Attribute to change (if specified)
+        this.showCursor = !this.isInput && this.options.showCursor;  // Only show cursor if it's not an input field
+        this.contentType = this.options.contentType;  // Type of content (text or HTML)
+        this.typeSpeed = this.options.typeSpeed;  // Speed of typing
+        this.startDelay = this.options.startDelay;  // Delay before starting typing
+        this.backSpeed = this.options.backSpeed;  // Speed of backspacing
+        this.backDelay = this.options.backDelay;  // Delay before backspacing starts
+        this.strings = this.options.strings;  // Array of strings to type
+        this.currentStringPos = 0;  // Current position in the string
+        this.currentArrayPos = 0;  // Current position in the array of strings
+        this.loopCount = this.options.loopCount;  // Number of times to loop
+        this.currentLoop = 0;  // Current loop count
+        this.isStopped = false;  // Flag to stop the typing
+        this.cursorChar = this.options.cursorChar;  // Cursor character
         this.build();
     };
 
-    s.prototype = {
-        constructor: s,
+    // Prototype methods for the TypedEffect
+    TypedEffect.prototype = {
+        constructor: TypedEffect,
+
+        // Initialize the typing effect
         init: function() {
-            let t = this;
-            t.timeout = setTimeout(function() {
-                t.typewrite(t.strings[t.arrayPos], t.strPos);
-            }, t.startDelay);
+            const self = this;
+            self.timeout = setTimeout(function() {
+                self.typewrite(self.strings[self.currentArrayPos], self.currentStringPos);
+            }, self.startDelay);  // Start typing after the delay
         },
+
+        // Build the cursor and initialize typing
         build: function() {
-            if (this.showCursor === !0) {
-                this.cursor = t('<span class="typed-cursor">' + this.cursorChar + "</span>");
-                this.el.after(this.cursor);
+            if (this.showCursor) {
+                this.cursor = $('<span class="typed-cursor">' + this.cursorChar + "</span>");
+                this.element.after(this.cursor);  // Add the cursor after the target element
             }
-            this.init();
+            this.init();  // Start the typing effect
         },
-        typewrite: function(t, s) {
-            if (this.stop !== !0) {
-                const o = Math.round(70 * Math.random()) + this.typeSpeed;
-                const e = this;
-                e.timeout = setTimeout(function() {
-                    let o = 0;
-                    let i = t.substr(s);
-                    if ("^" === i.charAt(0) && /^\^\d+/.test(i)) {
-                        let r;
-                        r = (i.match(/^\^\d+/)[0]).replace(/\^/, "");
-                        // i = i.substring(r.length);
-                        o = parseInt(r);
+
+        // Type each character
+        typewrite: function(currentString, currentPos) {
+            if (!this.isStopped) {
+                const randomDelay = Math.round(70 * Math.random()) + this.typeSpeed;  // Random delay for more human-like typing
+                const self = this;
+
+                self.timeout = setTimeout(function() {
+                    let delayAdjustment = 0;
+                    const remainingString = currentString.substr(currentPos);  // Remaining part of the string
+
+                    // Handle delay commands in the string (e.g., "^1000")
+                    if (remainingString.charAt(0) === "^" && /^\^\d+/.test(remainingString)) {
+                        const delayValue = remainingString.match(/^\^\d+/)[0].replace("^", "");
+                        delayAdjustment = parseInt(delayValue);
                     }
-                    if ("html" === e.contentType) {
-                        let n = t.substr(s).charAt(0);
-                        if ("<" === n || "&" === n) {
-                            let a = "";
-                            let h;
-                            for (h = "<" === n ? ">" : ";"; t.substr(s).charAt(0) !== h;) {
-                                a += t.substr(s).charAt(0);
-                                s++;
+
+                    // Handle HTML tags or entities
+                    if (self.contentType === "html") {
+                        const charAtPosition = currentString.substr(currentPos).charAt(0);
+                        if (charAtPosition === "<" || charAtPosition === "&") {
+                            let htmlSnippet = "";
+                            const endChar = charAtPosition === "<" ? ">" : ";";
+                            while (currentString.substr(currentPos).charAt(0) !== endChar) {
+                                htmlSnippet += currentString.substr(currentPos).charAt(0);
+                                currentPos++;
                             }
-                            s++;
-                            // a += h;
+                            currentPos++;
                         }
                     }
-                    e.timeout = setTimeout(function() {
-                        if (s === t.length) {
-                            e.options.onStringTyped(e.arrayPos);
 
-                            if (e.arrayPos === e.strings.length - 1) {
-                                e.options.callback();
-                                e.curLoop++;
+                    // Continue typing the string
+                    self.timeout = setTimeout(function() {
+                        if (currentPos === currentString.length) {
+                            self.options.onStringTyped(self.currentArrayPos);  // String fully typed
 
-                                if (e.loop === false || e.curLoop === e.loopCount) {
-                                    return;
+                            // Handle looping
+                            if (self.currentArrayPos === self.strings.length - 1) {
+                                self.options.callback();  // Call the callback function
+                                self.currentLoop++;
+
+                                if (!self.options.loop || self.currentLoop === self.loopCount) {
+                                    return;  // Exit if not looping or loop count reached
                                 }
                             }
 
-                            e.timeout = setTimeout(function() {
-                                e.backspace(t, s);
-                            }, e.backDelay);
+                            // Start backspacing
+                            self.timeout = setTimeout(function() {
+                                self.backspace(currentString, currentPos);
+                            }, self.backDelay);
                         } else {
-                            if (0 === s)
-                                e.options.preStringTyped(e.arrayPos);
-                            let o = t.substr(0, s + 1);
-                            if (e.attr)
-                                e.el.attr(e.attr,o);
-                            else if (e.isInput)
-                                e.el.val(o);
-                            else if ("html" === e.contentType)
-                                e.el.html(o);
-                            else
-                                e.el.text(o);
+                            if (currentPos === 0) {
+                                self.options.preStringTyped(self.currentArrayPos);  // Before typing each string
+                            }
 
-                            s++;
-                            e.typewrite(t, s);
+                            const currentSubstr = currentString.substr(0, currentPos + 1);  // Get substring up to current position
+                            if (self.attribute) {
+                                self.element.attr(self.attribute, currentSubstr);
+                            } else if (self.isInput) {
+                                self.element.val(currentSubstr);  // Update value for input elements
+                            } else if (self.contentType === "html") {
+                                self.element.html(currentSubstr);  // Update HTML content
+                            } else {
+                                self.element.text(currentSubstr);  // Update text content
+                            }
+
+                            currentPos++;  // Move to next character
+                            self.typewrite(currentString, currentPos);  // Continue typing
                         }
-                    }, o);
-                }, o);
+                    }, delayAdjustment || randomDelay);
+                }, randomDelay);
             }
         },
-        backspace: function(t, s) {
-            if (this.stop !== !0) {
-                let o = Math.round(50 * Math.random()) + this.backSpeed;
-                let e = this;
-                e.timeout = setTimeout(function() {
-                    if ("html" === e.contentType && ">" === t.substr(s).charAt(0)) {
-                        let o = "";
-                        for (; "<" !== t.substr(s).charAt(0);) {
-                            o -= t.substr(s).charAt(0);
-                            s--;
-                        }
-                        s--;
-                        // o += "<";
-                    }
-                    let i = t.substr(0, s);
-                    if (e.attr)
-                        e.el.attr(e.attr, i);
-                    else if (e.isInput)
-                        e.el.val(i);
-                    else if ("html" === e.contentType)
-                        e.el.html(i);
-                    else
-                        e.el.text(i);
 
-                    if (s > e.stopNum) {
-                        s--;
-                        e.backspace(t, s);
-                    } else if (s <= e.stopNum) {
-                        e.arrayPos++;
-                        if (e.arrayPos === e.strings.length) {
-                            e.arrayPos = 0;
-                            e.init();
+        // Backspace function to delete characters
+        backspace: function(currentString, currentPos) {
+            if (!this.isStopped) {
+                const randomDelay = Math.round(50 * Math.random()) + this.backSpeed;
+                const self = this;
+
+                self.timeout = setTimeout(function() {
+                    if (self.contentType === "html" && currentString.substr(currentPos).charAt(0) === ">") {
+                        let htmlSnippet = "";
+                        while (currentString.substr(currentPos).charAt(0) !== "<") {
+                            htmlSnippet -= currentString.substr(currentPos).charAt(0);
+                            currentPos--;
+                        }
+                        currentPos--;
+                    }
+
+                    const updatedString = currentString.substr(0, currentPos);  // Get updated substring
+                    if (self.attribute) {
+                        self.element.attr(self.attribute, updatedString);
+                    } else if (self.isInput) {
+                        self.element.val(updatedString);  // Update value for input
+                    } else if (self.contentType === "html") {
+                        self.element.html(updatedString);  // Update HTML
+                    } else {
+                        self.element.text(updatedString);  // Update text content
+                    }
+
+                    if (currentPos > self.stopNum) {
+                        currentPos--;  // Move back one character
+                        self.backspace(currentString, currentPos);  // Continue backspacing
+                    } else {
+                        self.currentArrayPos++;
+                        if (self.currentArrayPos === self.strings.length) {
+                            self.currentArrayPos = 0;  // Reset to first string if looped
+                            self.init();  // Start over
                         } else {
-                            e.typewrite(e.strings[e.arrayPos], s);
+                            self.typewrite(self.strings[self.currentArrayPos], currentPos);  // Start typing next string
                         }
                     }
-                }, o);
+                }, randomDelay);
             }
         },
+
+        // Reset the typed effect
         reset: function() {
-            let t = this;
-            clearInterval(t.timeout);
-            let s = this.el.attr("id");
-            this.el.after('<span id="' + s + '"/>');
-            this.el.remove();
-            if (typeof this.cursor !== "undefined")
-                this.cursor.remove();
-            t.options.resetCallback();
+            clearInterval(this.timeout);  // Clear any ongoing timeouts
+            const elementId = this.element.attr("id");
+            this.element.after('<span id="' + elementId + '"/>');
+            this.element.remove();  // Remove the current element
+            if (typeof this.cursor !== "undefined") {
+                this.cursor.remove();  // Remove the cursor if it exists
+            }
+            this.options.resetCallback();  // Call the reset callback
         }
     };
 
-    t.fn.typed = function(o) {
+    // jQuery plugin definition
+    $.fn.typed = function(option) {
         return this.each(function() {
-            let e = t(this);
-            let i = e.data("typed");
-            let r = typeof o === "object" && o;
-            if (!i) {
-                e.data("typed", i = new s(this, r));
+            const element = $(this);
+            let typedInstance = element.data("typed");
+            const options = typeof option === "object" && option;
+
+            if (!typedInstance) {
+                element.data("typed", new TypedEffect(this, options));  // Create new instance
             }
-            if (typeof o === "string") {
-                i[o]();
+
+            if (typeof option === "string") {
+                typedInstance[option]();  // Call method if string option passed
             }
         });
     };
 
-    t.fn.typed.defaults = {
+    // Default options for the plugin
+    $.fn.typed.defaults = {
         typeSpeed: 30,
         startDelay: 0,
         backSpeed: 10,
         backDelay: 2000,
-        loop: !1,
-        loopCount: !1,
-        showCursor: !0,
+        loop: false,
+        loopCount: false,
+        showCursor: true,
         cursorChar: "|",
         attr: null,
         contentType: "html",
@@ -182,24 +207,30 @@
         onStringTyped: function() {},
         resetCallback: function() {}
     };
+
 }(window.jQuery);
 
+// Function to re-initialize the typed effect
 function newTyped() {}
+
+// Initialize the typed effect on page load
 $(function() {
     $("#typed").typed({
-        // Change to edit type effect
-        strings: ["From the land of ramen and cherry blossoms, now residing in the land of saunas and reindeers!",
+        strings: [
+            "From the land of ramen and cherry blossoms, now residing in the land of saunas and reindeers!",
             "Mastering the art of coding and coffee consumption at Tampere University of Applied Sciences.",
             "Been rockin' the service sector as an entrepreneur in Tampere, Finland for 15 years.",
             "Balancing coding with a love for cooking and travel, my tech journey seamlessly combines software development and exploration.",
-            "From crafting software to savoring hand-ground coffee, I blend my passion for technology with life's adventures."],
-        contentType: "html",
-        loop: true,
+            "From crafting software to savoring hand-ground coffee, I blend my passion for technology with life's adventures."
+        ],
+        contentType: "html",  // Use HTML content type
+        loop: true,  // Loop the strings
         resetCallback: function() {
-            newTyped();
+            newTyped();  // Reset the typed effect when reset button is clicked
         }
     });
 
+    // Reset button functionality
     $(".reset").click(function() {
         $("#typed").typed("reset");
     });
